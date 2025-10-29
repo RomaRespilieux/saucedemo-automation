042a087ca43f8cfb1b9ec94ccf2d8cebcb027adb
Roma Respilieux <roma.respilieux@brightest.be>
2025-10-29 14:53:42 +0100

    shopping cart tests

---
+++ cypress/e2e/shoppingcart.cy.js
@@ -0,0 +1,121 @@
+// prerequisite = user is logged in
+function login() {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+}
+
+
+
+describe('shopping cart functionality', () => {
+    // add 1 item in cart
+    it('adding 1 item in cart', () => {
+        login();
+
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+
+        cy.get('.shopping_cart_badge').should('contain', '1');
+    });
+    // add 2 items in cart
+    it('adding multiple items in cart', () => {
+        login();
+
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('#add-to-cart-sauce-labs-bike-light').click();
+
+        cy.get('.shopping_cart_badge').should('contain', '2');
+
+    });
+});
+
+describe('shopping basket functionality', () => {
+    // remove 1 item from cart
+    it('remove 1 item', () => {
+        login();
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('#add-to-cart-sauce-labs-bike-light').click();
+
+        cy.get('.shopping_cart_link').click();
+        cy.get('#remove-sauce-labs-backpack').click();
+
+        //assertion
+        cy.get('#remove-sauce-labs-backpack').should('not.exist');
+    });
+
+    // empty cart
+    it('remove all items', () => {
+        login();
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('#add-to-cart-sauce-labs-bike-light').click();
+
+        cy.get('.shopping_cart_link').click();
+        cy.get('#remove-sauce-labs-backpack').click();
+        cy.get('#remove-sauce-labs-bike-light').click();
+
+        //assertion
+        cy.get('#remove-sauce-labs-backpack').should('not.exist');
+        cy.get('#remove-sauce-labs-backpack').should('not.exist');
+        cy.get('#remove-sauce-labs-bike-light').should('not.exist');
+
+    });
+});
+
+describe('Buying products', () => {
+
+    //1 product
+    it('buying 1 product', () => {
+        login();
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('.shopping_cart_link').click();
+        cy.get('#checkout').click();
+
+        cy.get('#first-name').type("Test");
+        cy.get('#last-name').type("User");
+        cy.get('#postal-code').type("1000");
+        cy.get('#continue').click();
+        cy.get('#finish').click();
+
+        cy.get('.pony_express').should('be.visible');
+
+        
+    });
+
+    //2 products
+    it('buying 2 products', () => {
+        login();
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('#add-to-cart-sauce-labs-bike-light').click();
+        cy.get('.shopping_cart_link').click();
+        cy.get('#checkout').click();
+
+        cy.get('#first-name').type("Test");
+        cy.get('#last-name').type("User");
+        cy.get('#postal-code').type("1000");
+        cy.get('#continue').click();
+        cy.get('#finish').click();
+
+        cy.get('.pony_express').should('be.visible');
+        
+    });
+});
+
+// cancel payment
+
+describe('Cancel payment', () => {
+    it('Should go back to the shopping page', () => {
+        login();
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('.shopping_cart_link').click();
+        cy.get('#checkout').click();
+
+        cy.get('#first-name').type("Test");
+        cy.get('#last-name').type("User");
+        cy.get('#postal-code').type("1000");
+        cy.get('#continue').click();
+        cy.get('#cancel').click();
+
+        cy.get('.app_logo').should('be.visible');
+
+    });
+});
